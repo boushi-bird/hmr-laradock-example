34b7a7b089d37860c35d7595a5bf0991918c9798
boushi-bird <boushi-bird@users.noreply.github.com>
2018-02-04 20:02:04 +0900

    コンテナ側でHMRするための設定追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,14 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080
+    },
+    watchOptions: {
+        poll: 2000,
+        ignored: /node_modules/
+    }
+});
